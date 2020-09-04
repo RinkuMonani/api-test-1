0b1051f26f97a6f99010dd5b2602d90b779cd121
RinkuMonani <monanira@gmail.com>
2020-09-04 16:59:19 +0530

    registed models to admin

--- rental/admin.py
+++ rental/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Friend)
+admin.site.register(models.Belonging)
+admin.site.register(models.Borrowed)
